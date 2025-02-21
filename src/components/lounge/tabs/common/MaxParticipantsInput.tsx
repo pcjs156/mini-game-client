e7d4037fdceb47fe8a48e84ca5ac49a6ebbcbc0a
pcjs156 <qkrdbqls1001@gmail.com>
2025-02-21 10:28:31 +0900

    feat: 게임 최대 참여 인원 설정 UI

---
+++ src/components/lounge/tabs/common/MaxParticipantsInput.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import { GameType } from "../../../../types/room";
+
+interface MaxParticipantsInputProps {
+  gameType: GameType;
+}
+
+const fixedMaxParticipants: Record<GameType, number> = {
+  "tic-tac-toe": 2,
+};
+
+export default function MaxParticipantsInput({ gameType }: MaxParticipantsInputProps) {
+  const [playerCount, setPlayerCount] = useState<number>(0);
+  const [isModifiable, setIsModifiable] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (fixedMaxParticipants[gameType]) {
+      setPlayerCount(fixedMaxParticipants[gameType]);
+      setIsModifiable(false);
+    } else {
+      setPlayerCount(0);
+      setIsModifiable(true);
+    }
+  }, [gameType]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (/^\d*$/.test(value)) {
+      setPlayerCount(Number.parseInt(value));
+    }
+  };
+
+  return (
+    <Box sx={{ mt: 2 }}>
+      <TextField
+        fullWidth
+        label="참여 인원"
+        value={playerCount}
+        onChange={handleChange}
+        type="number"
+        disabled={!isModifiable}
+        inputProps={{ min: 1, max: fixedMaxParticipants[gameType] || 10 }}
+        margin="normal"
+      />
+    </Box>
+  );
+}
