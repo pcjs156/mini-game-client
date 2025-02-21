4418699d3a728324c93c62cdb0291539f19570cd
pcjs156 <qkrdbqls1001@gmail.com>
2025-02-21 10:28:41 +0900

    feat: 틱택토 게임 생성 UI

--- src/components/lounge/tabs/TicTacToeTab.tsx
+++ src/components/lounge/tabs/TicTacToeTab.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import PlayerCountInput from "./common/MaxParticipantsInput";
 
 const TicTacToeTab: React.FC = () => {
   return (
     <Box sx={{ mt: 2 }}>
-      <Typography variant="body1">틱택토 설정</Typography>
-      {/* 틱택토 관련 설정 추가 가능 */}
+      <PlayerCountInput gameType="tic-tac-toe" />
     </Box>
   );
 };
